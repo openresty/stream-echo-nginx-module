6e69f3b4f131781f0065cb967abbd989735d4999
Yichun Zhang (agentzh) <agentzh@gmail.com>
2015-10-25 10:19:10 +0800

    use the prefix "stream echo" instead of "echo" in the ddebug output.

--- src/ddebug.h
+++ src/ddebug.h
@@ -16,7 +16,7 @@
 
 #   if (NGX_HAVE_VARIADIC_MACROS)
 
-#       define dd(...) fprintf(stderr, "echo *** %s: ", __func__); \
+#       define dd(...) fprintf(stderr, "stream echo *** %s: ", __func__); \
             fprintf(stderr, __VA_ARGS__); \
             fprintf(stderr, " at %s line %d.\n", __FILE__, __LINE__)
 
